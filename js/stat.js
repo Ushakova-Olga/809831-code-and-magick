a97d58fc8a5301ed6ffa76b9c7c279c6661d0dc0
Ushakova-Olga <Ushakova-Olga@yandex.ru>
2018-11-21 13:55:22 +0700

    Исправление ошибок по заданию 2

--- js/stat.js
+++ js/stat.js
@@ -1,27 +1,72 @@
 'use strict';
 
-var renderStatistics = function (ctx, names, times) { // eslint-disable-line no-unused-vars
-  /* проверка ругается на то что функция не вызывается в этом же файле.
-  как исправить это я не знаю, вызывать ее здесь незачем.
-  поэтому отключила эту строку от проверки */
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 420, 270);
-  ctx.fillStyle = 'rgb(255, 255, 255)';
-  ctx.fillRect(100, 10, 420, 270);
-  ctx.baseline = 'hanging';
-  ctx.font = '20px Tahoma';
-  ctx.fillStyle = 'rgb(0, 0, 0)';
-  ctx.fillText('Ура вы победили!', 220, 40);
-  ctx.fillText('Список результатов:', 210, 60);
+/* Сделала по демке с использованием констант*/
+var CLOUD_HEIGHT = 270;
+var GAP = 10;
+var MAX_BAR_HEIGHT = 150;
+var BAR_WIDTH = 40;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var TEXT_HEIGHT = 20;
 
+var renderCloud = function (ctx, x, y, color, cloudWidth) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, cloudWidth, CLOUD_HEIGHT);
+};
+
+var getMaxElement = function (arr) {
   var max = 0;
-  for (var i = 0; i < names.length; i++) {
-    if (times[i] > max) {
-      max = times[i];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  /* Из демки поняла что надо бы еще массивы привести к одинаковой длине, если они по ошибке переданы разной длины
+  Протестировала, добавив несколько элементов в names и наоборот в times*/
+  var i = 0;
+  var cloudWidth = 420;
+  if (names.length < times.length) {
+    for (i = names.length; i < times.length; i++) {
+      names[i] = 'Pl' + i;
     }
   }
 
-  for (i = 0; i < names.length; i++) {
+  if (names.length > times.length) {
+    for (i = times.length; i < names.length; i++) {
+      times[i] = 0;
+    }
+  }
+
+  var lengthArray = names.length;
+  var maxTime = getMaxElement(times);
+  var barGap = Math.floor((cloudWidth - lengthArray * BAR_WIDTH) / (lengthArray + 1));
+
+  /* Поскольку массив может быть разной длины, логично, что облако должно иметь возможность
+  расширяться при увеличинии числа игроков
+  Об этом ничего не сказано в ТЗ, ширина облака там фиксирована и равна 420.
+  Не знаю правильно ли сделала.
+  Хотела константу CLOUD_WIDTH переопределить здесь, но так понимаю это противоречит самому принципу констант,
+  поэтому завела переменную cloudWidth внутри функции */
+
+  if ((cloudWidth - lengthArray * BAR_WIDTH) <= 0) {
+    cloudWidth = lengthArray * BAR_WIDTH + GAP * (lengthArray + 1);
+    barGap = Math.floor((cloudWidth - names.length * BAR_WIDTH) / (lengthArray + 1));
+  }
+
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)', cloudWidth);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, 'rgb(255, 255, 255)', cloudWidth);
+  ctx.baseline = 'hanging';
+  ctx.font = '20px Tahoma';
+  ctx.fillStyle = 'rgb(0, 0, 0)';
+  /* Числа 180 и 200 здесь это ширина текста, если нужно могу тоже вынести в константы */
+  ctx.fillText('Ура вы победили!', CLOUD_X + (cloudWidth - 180) / 2, 2 * TEXT_HEIGHT);
+  ctx.fillText('Список результатов:', CLOUD_X + (cloudWidth - 200) / 2, 3 * TEXT_HEIGHT);
+
+  for (i = 0; i < lengthArray; i++) {
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
@@ -31,9 +76,9 @@ var renderStatistics = function (ctx, names, times) { // eslint-disable-line no-
       }
       ctx.fillStyle = 'rgba(0, 0, 255,' + color + ')';
     }
-    ctx.fillRect(170 + i * 80, 250 - 150 * times[i] / max, 40, 150 * times[i] / max);
+    ctx.fillRect(CLOUD_X + barGap + i * (barGap + BAR_WIDTH), CLOUD_HEIGHT - 2 * GAP - MAX_BAR_HEIGHT * times[i] / maxTime, BAR_WIDTH, MAX_BAR_HEIGHT * times[i] / maxTime);
     ctx.fillStyle = 'rgb(0, 0, 0)';
-    ctx.fillText(names[i], 170 + i * 80, 270);
-    ctx.fillText(Math.floor(times[i]), 170 + i * 80, 240 - 150 * times[i] / max);
+    ctx.fillText(names[i], CLOUD_X + barGap + i * (barGap + BAR_WIDTH), CLOUD_HEIGHT);
+    ctx.fillText(Math.floor(times[i]), CLOUD_X + barGap + i * (barGap + BAR_WIDTH), CLOUD_HEIGHT - 3 * GAP - MAX_BAR_HEIGHT * times[i] / maxTime);
   }
 };
